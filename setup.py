f74a33920ec09ddb01eb7215b446d4b35441c8f5
Aaron Rosenfeld <aaron.m.rosenfeld@gmail.com>
2022-10-27 15:10:14 -0400

    updates for pip

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as fh:
+    install_requires = fh.read().splitlines()
+
+
 setup(
     name='hicutils',
     version='0.0.1',
@@ -13,9 +17,5 @@ setup(
         'hicutils',
         'hicutils.core',
     ],
-    install_requires=[
-        'pandas>=1.5.1',
-        'seaborn>=0.12.1',
-        'scipy>=1.9.3'
-    ],
+    install_requires=install_requires
 )
