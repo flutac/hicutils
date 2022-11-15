4326e01839b26b3966103b785d38280a43c0f6f1
Aaron Rosenfeld <aaron.m.rosenfeld@gmail.com>
2022-11-15 09:02:21 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,4 @@ set -e
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-git clone https://github.com/PennHIC/hicutils.git
-cd hicutils
-pip install -e .
-deactivate
+pip install -e hicutils
