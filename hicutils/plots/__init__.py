209656bf8004713d9a22a3dfe854e4a5f78d8a52
Aaron Rosenfeld <aaron.m.rosenfeld@gmail.com>
2022-11-17 15:14:00 -0500

    upset plots

--- hicutils/plots/__init__.py
+++ hicutils/plots/__init__.py
@@ -3,7 +3,10 @@ from .clone_size import (  # noqa: F401
     plot_top_clones,
     plot_ranges
 )
-from .overlap import plot_strings  # noqa: F401
+from .overlap import (  # noqa: F401
+    plot_strings,
+    plot_upset
+)
 from .gene_usage import plot_gene_usage  # noqa: F401
 from .cdr3_analysis import (  # noqa: F401
     plot_cdr3_aa_usage,
@@ -15,4 +18,4 @@ from .shm import (  # noqa: F401
     plot_shm_aggregate,
     plot_shm_range
 )
-from .upset import plot_upset  # noqa: F401
+from .upset import   # noqa: F401
